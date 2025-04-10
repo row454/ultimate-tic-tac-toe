a6220cea61331b0d1715eec3a4ef23bd3273eb52
row454 <56337058+row454@users.noreply.github.com>
2025-04-10 15:19:33 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ fn Menu() -> impl IntoView {
                     <button on:click=move |_| {set_gamemode.set(Some(Gamemode::Host))}>Play Online</button>
                     <div class="slidecontainer">
                      <input type="range" min="-3000" max="477" value="477" class="slider" id="myRange"
-                        on:input:target=move |ev| {
+                        on:input=move |ev| {
             // .value() returns the current value of an HTML input element
             set_difficulty.set(ev.target().value());
         }      prop:value=difficulty
